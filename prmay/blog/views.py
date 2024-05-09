09aca977b27a8fbb37a15940075f4c994bed1d6f
lileshp <35130575+lileshp@users.noreply.github.com>
2024-05-09 12:36:02 +0530

    Add files via upload

--- prmay/blog/views.py
+++ prmay/blog/views.py
@@ -6,8 +6,21 @@ def home(request):
     return HttpResponse("HOME PAGE")
 
 def msg(request):
-    return HttpResponse("BLOG MESSAGES!")
+    data = { 
+        'title':"WARRIORS", 
+        'players':['john','sam','peter','mac','johny'],
+        'points':[80,60,40,50,75,45,30,20,10,85,90,76,42],
+        'teams':[
+            {'name':'TEAM1','city':'BHOPAL'},
+            {'name':"TEAM2",'city':"DELHI"}
+            ]}
+    # return HttpResponse("BLOG MESSAGES!")
+    return render(request, 'message.html',data)
 
 def blog1(request):
     # return HttpResponse("BLOGs HOME PAGE")
-    return render(request,'home.html')
+    name = 'JOHN'
+    college = "lnct"
+    branch = "Computer Science"
+    std_details = {'name':name,'clg':college,'branch':branch}
+    return render(request,'home.html',std_details)
--- prmay/django_class_apr24/prmay/prmay/settings.py
+++ prmay/django_class_apr24/prmay/prmay/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_DIR = BASE_DIR/'static'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -131,8 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [ STATIC_DIR ]
+STATIC_URL = 'static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
